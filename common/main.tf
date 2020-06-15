7eb0f15ae20a1b7f9c8e0b1812210bf633ad0a9f
CAMHub PublishBot <octravis@us.ibm.com>
2020-06-15 23:25:36 +0000

    Automated CAMHub::publish on 2020-06-15 23:25:36 UTC

---
+++ common/main.tf
@@ -0,0 +1,17 @@
+# This is a terraform generated template generated from blueprint89
+
+##############################################################
+# Keys - CAMC (public/private) & optional User Key (public) 
+##############################################################
+variable "allow_unverified_ssl" {
+  description = "Communication with vsphere server with self signed certificate"
+  default     = "true"
+}
+
+##############################################################
+# Define the vsphere provider 
+##############################################################
+provider "vsphere" {
+  allow_unverified_ssl = "${var.allow_unverified_ssl}"
+  
+}
